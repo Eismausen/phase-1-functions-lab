9cbfca80f396950c00e225660cc2eddfe2289e6d
David Avery <eismausen@gmail.com>
2021-10-13 09:09:29 -0700

    commit tree 32739ff9572ca58623e0f200c0eb613656362856

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    return Math.abs(pickup-42);
+}
+
+function distanceFromHqInFeet(end) {
+    let blocks = distanceFromHqInBlocks(end);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    let blocks = Math.abs(start - end);
+    return blocks * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400) {
+        return 0;
+    } else if (distance < 2000) {
+        return (distance - 400) * .02;
+    } else if (distance < 2500) {
+        return 25.00;
+    } else {
+        return 'cannot travel that far';
+    }
+
+}
